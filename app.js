6b66d96955c04983043a7d7affa907c50487191a
Saif Azharuddin <saifA.uddin@gmail.com>
2022-06-20 16:38:19 -0700

    whole bunch of changes to conditional logic

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const scoreboard = document.querySelector('#scoreboard');
 const scoreSelect = document.querySelector('#scoreLimit');
-const pointP1 = document.querySelector('#pointP1');
-const pointP2 = document.querySelector('#pointP2');
-const scoreP1 = 0;
-const scoreP2 = 0;
+var maxScore = parseInt(document.querySelector('#scoreLimit option:checked').value);
+var pointP1 = document.querySelector('#pointP1');
+var pointP2 = document.querySelector('#pointP2');
+var scoreP1 = 0;
+var scoreP2 = 0;
+
+scoreSelect.addEventListener('change', function () {
+    maxScore = parseInt(document.querySelector('#scoreLimit option:checked').value);
+})
 
 if (scoreP1 < maxScore && scoreP2 < maxScore) {
     pointP1.addEventListener('click', function () {
         scoreP1++;
+        scoreboard.innerText = `${scoreP1} to ${scoreP2}`;
     })
     pointP2.addEventListener('click', function () {
         scoreP2++;
+        scoreboard.innerText = `${scoreP1} to ${scoreP2}`;
     })
 }
+
